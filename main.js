42226690e6c3ea2abce6e1b582bd145b3254f976
Tomasz Kula <tomaszkula07@gmail.com>
2016-07-05 17:18:25 +0200

    Grid

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
+
+var ticTac = {
+
+  board : [[1,2,3],[4,5,6],[7,8,9]],
+
+  // player1 : "x",
+  // player2 : { sign : 'y' },
+  // // board : [['','',''],['','',''],['','','']],
+  // currentSign : this.player2['sign'],
+
+  // changeSign : function (){
+  //   this.currentSign = (currentSign == this.player1.sign) ? this.player2.sign : this.player1.sign;
+  // },
+
+  updateBoard : function() {
+    // flatten arr
+    let board = [].concat(...this.board);
+
+    //fill with current content
+    board.forEach(function(content, index){
+      $('#' + (index + 1)).text(content);
+    });
+  }
+};
+
+
 function playIntro(){
   // show
   let duration = 1000;
@@ -19,13 +45,17 @@ function playIntro(){
 
 }
 
-
 function drawBoard(){
   $('.board-container').fadeIn('slow');
 }
 
 $(document).ready(function(){
-  playIntro();
+  // playIntro();
   drawBoard();
+  ticTac.updateBoard();
 
+
+  $('.board-container').on('click', 'p', function(){
+    console.log('test');
+  });
 });
